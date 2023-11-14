337559a43f0ced919e35da61f59924b98560865b
sachin-jk <134486782+sachin-jk@users.noreply.github.com>
2023-11-14 22:55:02 +0530

    Update install-jfrog-ubuntu.sh

--- install-jfrog-ubuntu.sh
+++ install-jfrog-ubuntu.sh
@@ -25,7 +25,7 @@ echo "*****Downloading JFROG Artifactory 6.23.42 version"
 sudo systemctl stop artifactory > /dev/null 2>&1
 cd /opt 
 sudo rm -rf jfrog* artifactory*
-sudo wget -q https://jfrog.com/community/open-source/jfrog-artifactory-oss-6.23.42.zip
+sudo wget -q https://releases.jfrog.io/artifactory/bintray-artifactory/org/artifactory/oss/jfrog-artifactory-oss/6.23.42/jfrog-artifactory-oss-6.23.42.zip
 sudo unzip -q jfrog-artifactory-oss-6.23.42.zip -d /opt/artifactory 1>/dev/null
 sudo chown -R artifactory: /opt/artifactory/*
 sudo rm -rf jfrog-artifactory-oss-6.23.42.zip
